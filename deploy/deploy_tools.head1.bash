ae45669ede9e7ebf18cc9f9d8ff07fe9394c4b89
Jon Thunder <John.Thunder@everwonder.us>
2019-12-06 16:56:49 +0000

    Logic typo

--- deploy/deploy_tools.head1.bash
+++ deploy/deploy_tools.head1.bash
@@ -8,7 +8,7 @@ source_files() {
   f=source_me.bash ; [[ -f $f ]] && source $f || echo "WARNING: Found no $f" 1>&2
   f=custom_deploy_source.bash ; [[ ! -f $f ]] || source $f
   if [[ $BUNDLE ]] ; then
-    if [[ $BUNDLE != 'bundle-prod' ]] ; then
+    if [[ $BUNDLE == 'bundle-prod' ]] ; then
       if [[ ! -e source_me.prod.bash ]] ; then
         die "ERROR: BUNDLE = $BUNDLE but found no source_me.prod.bash with production-worthy passwords and configurations"
       fi ;
