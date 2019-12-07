2c94b0dc67e0098294000c29ed79e28fd53402eb
Jon Thunder <John.Thunder@everwonder.us>
2019-12-07 17:42:33 +0000

    Move source_files to the foot so that custom_deploy_source.bash can override the default functions (like post_apache_deploy)

--- deploy/deploy_tools.head1.bash
+++ deploy/deploy_tools.head1.bash
@@ -3,17 +3,4 @@
 # # #     curl https://raw.githubusercontent.com/JonThunder/deploy_tools_bash/master/mk_deploy.sh | bash
 
 die() { echo "${1:-ERROR}" 1>&2 ; exit ${2:-2} ; }
-source_files() {
-  f=source_me.bash ; [[ -f $f ]] && source $f || echo "WARNING: Found no $f" 1>&2
-  f=custom_deploy_source.bash ; [[ ! -f $f ]] || source $f
-  if [[ $BUNDLE ]] ; then
-    if [[ $BUNDLE == 'bundle-prod' ]] ; then
-      if [[ ! -e source_me.prod.bash ]] ; then
-        die "ERROR: BUNDLE = $BUNDLE but found no source_me.prod.bash with production-worthy passwords and configurations"
-      fi ;
-      source ./source_me.prod.bash
-    fi
-  fi
-}
-source_files
 
