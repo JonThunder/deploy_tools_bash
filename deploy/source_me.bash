92ba871559c3ca1168951dccfcfeb0d661250eec
Jon Thunder <John.Thunder@everwonder.us>
2019-12-07 01:30:25 +0000

    Defaults for DBU and DBUP for the sake of provision-db.sh

--- deploy/source_me.bash
+++ deploy/source_me.bash
@@ -3,5 +3,7 @@ ADMIN_USERS="user1"
 DOCKER_USERS="vagrant $ADMIN_USERS"
 DATABASES='my_db1'
 DB_ROOT_P='databaseR00tPW' # For example
+DBU='my_dbu1'
+DBUP='databaseY00$3rPW'
 FIX_STRINGS_IN_FILES="html/index.php
 "
