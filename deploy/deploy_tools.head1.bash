1254527d02abfe79d8ec71bc3f8fd608492cb282
Jon Thunder <John.Thunder@everwonder.us>
2019-12-10 01:09:05 +0000

    final()

--- deploy/deploy_tools.head1.bash
+++ deploy/deploy_tools.head1.bash
@@ -2,5 +2,7 @@
 # # #   To bootstrap a project that uses this for its deployments, run
 # # #     curl https://raw.githubusercontent.com/JonThunder/deploy_tools_bash/master/mk_deploy.sh | bash
 
-die() { echo "${1:-ERROR}" 1>&2 ; exit ${2:-2} ; }
-
+if ! LC_ALL=C type -t final | grep '^function$' ; then
+  final() { true ; }
+fi
+die() { echo "${1:-ERROR}" 1>&2 ; final; exit ${2:-2} ; }
